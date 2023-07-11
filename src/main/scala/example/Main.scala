95002e003b4ef4ea48daeba2b601dbec35544b1d
Anton Sviridov <keynmol@gmail.com>
2023-07-11 13:50:58 +0100

    titles and shit

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -58,100 +58,108 @@ def app =
   inline def poundValue(source: Source[Double]) =
     span(color.maroon, child.text <-- source.toObservable.map(d => f"£$d%.2f"))
 
-  div(
-    display.flex,
-    alignItems.center,
-    width := "100%",
-    div(
-      width := "50%",
-      h1("Rate: ", child.text <-- rate.signal.map(y => s"$y%")),
-      Slider(
-        _.min           := 2,
-        _.max           := 15.0,
-        _.step          := 1,
-        _.value         := 6,
-        _.showTooltip   := true,
-        _.labelInterval := 1,
-        _.showTickmarks := true,
-        _.events.onInput.map(_.target.value) --> rate
-      ),
-      h1("House price: ", poundValue(housePrice)),
-      Slider(
-        _.min           := 100_000,
-        _.max           := 1_200_000,
-        _.step          := 25_000,
-        _.value         := 400_000,
-        _.showTooltip   := true,
-        _.labelInterval := 10,
-        _.showTickmarks := true,
-        _.events.onInput.map(_.target.value) --> housePrice
-      ),
-      h1("Deposit: ", poundValue(deposit)),
-      Slider(
-        _.min           := 10_000,
-        _.max           := 300_000,
-        _.step          := 10_000,
-        _.value         := 50_000,
-        _.showTooltip   := true,
-        _.labelInterval := 5,
-        _.showTickmarks := true,
-        _.events.onInput.map(_.target.value) --> deposit
-      ),
-      h1(
-        "Duration: ",
-        span(
-          color.olive,
-          child.text <-- durationYears.signal.map(y => s"$y years")
-        )
-      ),
-      Slider(
-        _.min           := 10,
-        _.max           := 35,
-        _.step          := 1,
-        _.value         := 25,
-        _.showTooltip   := true,
-        _.labelInterval := 1,
-        _.showTickmarks := true,
-        _.events.onInput.map(_.target.value.toInt) --> durationYears
-      ),
-      h1(
-        "Monthly payment: ",
-        poundValue(calculation)
+  val sliders = div(
+    width := "50%",
+    h1("Rate: ", child.text <-- rate.signal.map(y => s"$y%")),
+    Slider(
+      _.min           := 2,
+      _.max           := 15.0,
+      _.step          := 1,
+      _.value         := 6,
+      _.showTooltip   := true,
+      _.labelInterval := 1,
+      _.showTickmarks := true,
+      _.events.onInput.map(_.target.value) --> rate
+    ),
+    h1("House price: ", poundValue(housePrice)),
+    Slider(
+      _.min           := 100_000,
+      _.max           := 1_200_000,
+      _.step          := 25_000,
+      _.value         := 400_000,
+      _.showTooltip   := true,
+      _.labelInterval := 10,
+      _.showTickmarks := true,
+      _.events.onInput.map(_.target.value) --> housePrice
+    ),
+    h1("Deposit: ", poundValue(deposit)),
+    Slider(
+      _.min           := 10_000,
+      _.max           := 300_000,
+      _.step          := 10_000,
+      _.value         := 50_000,
+      _.showTooltip   := true,
+      _.labelInterval := 5,
+      _.showTickmarks := true,
+      _.events.onInput.map(_.target.value) --> deposit
+    ),
+    h1(
+      "Duration: ",
+      span(
+        color.olive,
+        child.text <-- durationYears.signal.map(y => s"$y years")
       )
     ),
-    div(
-      width := "50%",
-      canvasTag( // onMountUnmount callback to bridge the Laminar world and the Chart.js world
-        onMountUnmountCallback(
-          // on mount, create the `Chart` instance and store it in optChart
-          mount = nodeCtx =>
-            val domCanvas: dom.HTMLCanvasElement = nodeCtx.thisNode.ref
-            val chart = new Chart(domCanvas, js.Dictionary("type" -> "line"))
-            optChart = Some(chart)
-          ,
-          // on unmount, destroy the `Chart` instance
-          unmount = thisNode =>
-            for chart <- optChart do chart.destroy()
-            optChart = None
-        ),
-        combined --> { settings =>
-          val deposits =
-            50_000.to(250_000.min(settings.housePrice.toInt), 5_000)
-          val monthlyPayments = deposits.map(dep =>
-            settings.copy(deposit = dep.toDouble).monthlyPayment
-          )
-          optChart.foreach { ch =>
-            ch.data.labels = js.Array(deposits.map(_.toString)*)
-            ch.data.datasets = js.Array(
-              ChartDataset(
-                "Monthly payments at various deposit amounts",
-                js.Array(monthlyPayments*)
-              )
+    Slider(
+      _.min           := 10,
+      _.max           := 35,
+      _.step          := 1,
+      _.value         := 25,
+      _.showTooltip   := true,
+      _.labelInterval := 1,
+      _.showTickmarks := true,
+      _.events.onInput.map(_.target.value.toInt) --> durationYears
+    ),
+    h1(
+      "Monthly payment: ",
+      poundValue(calculation)
+    )
+  )
+  val chart = div(
+    width := "50%",
+    canvasTag( // onMountUnmount callback to bridge the Laminar world and the Chart.js world
+      onMountUnmountCallback(
+        // on mount, create the `Chart` instance and store it in optChart
+        mount = nodeCtx =>
+          val domCanvas: dom.HTMLCanvasElement = nodeCtx.thisNode.ref
+          val chart = new Chart(domCanvas, js.Dictionary("type" -> "line"))
+          optChart = Some(chart)
+        ,
+        // on unmount, destroy the `Chart` instance
+        unmount = thisNode =>
+          for chart <- optChart do chart.destroy()
+          optChart = None
+      ),
+      combined --> { settings =>
+        val deposits =
+          50_000.to(250_000.min(settings.housePrice.toInt), 5_000)
+        val monthlyPayments = deposits.map(dep =>
+          settings.copy(deposit = dep.toDouble).monthlyPayment
+        )
+        optChart.foreach { ch =>
+          ch.data.labels = js.Array(deposits.map(_.toString)*)
+          ch.data.datasets = js.Array(
+            ChartDataset(
+              "Monthly payments at various deposit amounts",
+              js.Array(monthlyPayments*)
             )
-            ch.update()
-          }
+          )
+          ch.update()
         }
-      )
+      }
+    )
+  )
+  div(
+    h1(
+      "The lolno calculator - why you will never afford the mortgage in the UK"
+    ),
+    width := "100%",
+    div(
+      display.flex,
+      alignItems.center,
+      width := "100%",
+      sliders,
+      chart
     )
   )
 end app
